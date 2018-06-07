a8e14e5eeff1bc5f3c285e6c1f45b720008d5b5f
Michał Krasnoborski <mkrdln@gmail.com>
2018-06-08 01:24:20 +0200

    Reorder items in lib.rs
    This commit only rearanges the order of items in lib.rs.
    The layout now is more or less as follows:
    1. The run() entrypoint and some common structures,
    2. Preprocessing
    3. Scope analysis
    
    Perhaps the (2) and (3) should be moved into their own modules.

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,72 @@ use std::collections::BTreeSet as Set;
 use std::fs;
 use std::path::{Path, PathBuf};
 
+pub fn run(root_path: impl AsRef<Path>, emitter: &mut Emitter) -> Result<(), Error> {
+    run_(root_path.as_ref(), emitter)
+}
+
+fn run_(root_path: &Path, emitter: &mut Emitter) -> Result<(), Error> {
+    let mut files = Map::new();
+
+    for entry in WalkDir::new(root_path) {
+        let entry = entry.context("traversing")?;
+        if entry.path().to_str().unwrap_or("").contains("_Old_Tests") {
+            continue;
+        }
+        if !entry.file_type().is_file() {
+            continue;
+        }
+        if entry.path().extension().and_then(|ext| ext.to_str()) != Some("ps1") {
+            continue;
+        }
+
+        match parse_and_preprocess(entry.path(), emitter)? {
+            PreprocessOutput::Valid(parsed) => {
+                let path = entry.path().canonicalize()?;
+                files.insert(path, parsed);
+            }
+            PreprocessOutput::InvalidImports => {
+                eprintln!(
+                    "Stopping analysis for this file because of import errors: {}\n",
+                    entry.path().display()
+                );
+            }
+        };
+    }
+
+    analyze(&files, emitter).context("analyzing")?;
+
+    Ok(())
+}
+
+fn is_allowed(line: &str, what: &str) -> bool {
+    let mut chunks = line.splitn(2, "#");
+
+    match chunks.next() {
+        Some(_before_comment) => (),
+        None => return false,
+    }
+
+    match chunks.next() {
+        Some(comment) => comment.to_lowercase().contains("allow") && comment.contains(what),
+        None => false,
+    }
+}
+
+/// Kind of error message
+#[derive(Debug)]
+pub enum Message {
+    Error,
+    Warning,
+}
+
+impl Default for Message {
+    fn default() -> Message { Message::Error }
+}
+
 pub use syntax::Line;
 
+/// Location of a message
 #[derive(Default, Debug)]
 pub struct Location {
     pub file: PathBuf,
@@ -32,17 +96,6 @@ impl Line {
             file: file.to_owned(),
         }
     }
-}
-
-/// Parsed and preprocessed source file
-#[derive(Debug)]
-struct Parsed {
-    imports: Vec<PathBuf>,
-    definitions: Vec<syntax::Definition>,
-    usages: Vec<syntax::Usage>,
-
-    /// Original, non-resolved path, relative to PWD
-    original_path: PathBuf,
 }
 
 pub trait Emitter {
@@ -86,206 +139,17 @@ impl Emitter for VecEmitter {
     }
 }
 
-pub fn run(root_path: impl AsRef<Path>, emitter: &mut Emitter) -> Result<(), Error> {
-    run_(root_path.as_ref(), emitter)
-}
+// ---- Preprocessing --------------------------------------------------------
 
-pub fn run_(root_path: &Path, emitter: &mut Emitter) -> Result<(), Error> {
-    let mut files = Map::new();
-
-    for entry in WalkDir::new(root_path) {
-        let entry = entry.context("traversing")?;
-        if entry.path().to_str().unwrap_or("").contains("_Old_Tests") {
-            continue;
-        }
-        if !entry.file_type().is_file() {
-            continue;
-        }
-        if entry.path().extension().and_then(|ext| ext.to_str()) != Some("ps1") {
-            continue;
-        }
-
-        match parse_and_preprocess(entry.path(), emitter)? {
-            PreprocessOutput::Valid(parsed) => {
-                let path = entry.path().canonicalize()?;
-                files.insert(path, parsed);
-            }
-            PreprocessOutput::InvalidImports => {
-                eprintln!(
-                    "Stopping analysis for this file because of import errors: {}\n",
-                    entry.path().display()
-                );
-            }
-        };
-    }
-
-    analyze(&files, emitter).context("analyzing")?;
-
-    Ok(())
-}
-
-/// Functions in scope
-#[derive(Debug, Clone, Default)]
-struct Scope<'a> {
-    /// Functions defined in this scopeanalyze
-    defined: Set<&'a str>,
-    /// Defined by a file imported by `analyze
-    directly_imported: Set<&'a str>,
-    /// All the functions in scope
-    all: Set<&'a str>,
-    /// All the files imported (directlanalyzely)
-    files: Set<&'a Path>,
-}
-
-/// Type of function found in scope
+/// Parsed and preprocessed source file
 #[derive(Debug)]
-enum Found {
-    /// Found in Scope::defined or Scope::indirectly_imported
-    Direct,
-    /// Indirectly imported (through multiple layers of `.`)
-    Indirect,
-}
+struct Parsed {
+    imports: Vec<PathBuf>,
+    definitions: Vec<syntax::Definition>,
+    usages: Vec<syntax::Usage>,
 
-impl<'a> Scope<'a> {
-    fn search(&self, name: &str) -> Option<Found> {
-        if self.all.contains(name) {
-            if self.defined.contains(name) || self.directly_imported.contains(name) {
-                Some(Found::Direct)
-            } else {
-                Some(Found::Indirect)
-            }
-        } else {
-            None
-        }
-    }
-}
-
-/// State of scope computation
-#[derive(Debug, Clone)]
-enum ScopeWip<'a> {
-    /// Done
-    Resolved(Scope<'a>),
-
-    /// The scope is being currently computed
-    /// (used to detect import loop)
-    Current,
-}
-
-fn analyze(files: &Map<PathBuf, Parsed>, emitter: &mut Emitter) -> Result<(), Error> {
-    lazy_static! {
-        static ref BUILTINS: Set<&'static str> = include_str!("builtins.txt")
-            .split_whitespace()
-            .chain(include_str!("extras.txt").split_whitespace())
-            .collect();
-    }
-
-    let mut scopes = Map::new();
-
-    for path in files.keys() {
-        let scope = get_scope(path, files, &mut scopes)?;
-
-        let parsed = &files[path];
-
-        let mut already_analyzed = Set::new();
-
-        for usage in &parsed.usages {
-            if BUILTINS.contains(usage.name.as_str()) {
-                continue;
-            }
-            if is_allowed(&usage.location.line, &usage.name) {
-                continue;
-            }
-            if already_analyzed.contains(usage.name.as_str()) {
-                continue;
-            }
-
-            already_analyzed.insert(usage.name.as_str());
-
-            match scope.search(&usage.name) {
-                None => emitter.emit(
-                    Message::Error,
-                    format!("Not in scope: {}", usage.name),
-                    usage.location.in_file(&parsed.original_path),
-                    None,
-                ),
-                Some(Found::Indirect) => emitter.emit(
-                    Message::Warning,
-                    format!("Indirectly imported: {}", usage.name),
-                    usage.location.in_file(&parsed.original_path),
-                    None,
-                ),
-                _ => (),
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn is_allowed(line: &str, what: &str) -> bool {
-    let mut chunks = line.splitn(2, "#");
-
-    match chunks.next() {
-        Some(_before_comment) => (),
-        None => return false,
-    }
-
-    match chunks.next() {
-        Some(comment) => comment.to_lowercase().contains("allow") && comment.contains(what),
-        None => false,
-    }
-}
-
-fn get_scope<'a>(
-    file: &'a Path,
-    files: &'a Map<PathBuf, Parsed>,
-    scopes: &mut Map<&'a Path, ScopeWip<'a>>,
-) -> Result<Scope<'a>, Error> {
-    match scopes.get(file) {
-        Some(ScopeWip::Current) => bail!("Recursive import of {}", file.display()),
-        Some(ScopeWip::Resolved(scope)) => return Ok(scope.clone()),
-        _ => (),
-    };
-    scopes.insert(file, ScopeWip::Current);
-
-    let parsed_file = files.get(file).ok_or_else(|| {
-        format_err!(
-            "List of elements in scope of file {} was requested, \
-             but not available due to previous import error",
-            file.display()
-        )
-    })?;
-
-    let mut scope = Scope::default();
-
-    for import in &parsed_file.imports {
-        let nested = get_scope(&import, files, scopes)?;
-        scope.directly_imported.extend(&nested.defined);
-        scope.all.extend(&nested.all);
-        scope.files.extend(&nested.files);
-    }
-
-    for definition in &parsed_file.definitions {
-        scope.defined.insert(&definition.name);
-        scope.all.insert(&definition.name);
-    }
-
-    scope.files.insert(file);
-
-    scopes.insert(file, ScopeWip::Resolved(scope.clone()));
-
-    Ok(scope)
-}
-
-/// Kind of error message
-#[derive(Debug)]
-pub enum Message {
-    Error,
-    Warning,
-}
-
-impl Default for Message {
-    fn default() -> Message { Message::Error }
+    /// Original, non-resolved path, relative to PWD
+    original_path: PathBuf,
 }
 
 #[derive(Debug)]
@@ -392,4 +256,145 @@ fn resolve_imports(source_path: &Path, imports: Vec<syntax::Import>, emitter: &m
     }
 
     Ok(Some(resolved_imports))
+}
+
+// ---- Scope analysis -------------------------------------------------------
+
+/// Functions in scope
+#[derive(Debug, Clone, Default)]
+struct Scope<'a> {
+    /// Functions defined in this scopeanalyze
+    defined: Set<&'a str>,
+    /// Defined by a file imported by `analyze
+    directly_imported: Set<&'a str>,
+    /// All the functions in scope
+    all: Set<&'a str>,
+    /// All the files imported (directlanalyzely)
+    files: Set<&'a Path>,
+}
+
+/// Type of function found in scope
+#[derive(Debug)]
+enum Found {
+    /// Found in Scope::defined or Scope::indirectly_imported
+    Direct,
+    /// Indirectly imported (through multiple layers of `.`)
+    Indirect,
+}
+
+impl<'a> Scope<'a> {
+    fn search(&self, name: &str) -> Option<Found> {
+        if self.all.contains(name) {
+            if self.defined.contains(name) || self.directly_imported.contains(name) {
+                Some(Found::Direct)
+            } else {
+                Some(Found::Indirect)
+            }
+        } else {
+            None
+        }
+    }
+}
+
+/// State of scope computation
+#[derive(Debug, Clone)]
+enum ScopeWip<'a> {
+    /// Done
+    Resolved(Scope<'a>),
+
+    /// The scope is being currently computed
+    /// (used to detect import loop)
+    Current,
+}
+
+fn analyze(files: &Map<PathBuf, Parsed>, emitter: &mut Emitter) -> Result<(), Error> {
+    lazy_static! {
+        static ref BUILTINS: Set<&'static str> = include_str!("builtins.txt")
+            .split_whitespace()
+            .chain(include_str!("extras.txt").split_whitespace())
+            .collect();
+    }
+
+    let mut scopes = Map::new();
+
+    for path in files.keys() {
+        let scope = get_scope(path, files, &mut scopes)?;
+
+        let parsed = &files[path];
+
+        let mut already_analyzed = Set::new();
+
+        for usage in &parsed.usages {
+            if BUILTINS.contains(usage.name.as_str()) {
+                continue;
+            }
+            if is_allowed(&usage.location.line, &usage.name) {
+                continue;
+            }
+            if already_analyzed.contains(usage.name.as_str()) {
+                continue;
+            }
+
+            already_analyzed.insert(usage.name.as_str());
+
+            match scope.search(&usage.name) {
+                None => emitter.emit(
+                    Message::Error,
+                    format!("Not in scope: {}", usage.name),
+                    usage.location.in_file(&parsed.original_path),
+                    None,
+                ),
+                Some(Found::Indirect) => emitter.emit(
+                    Message::Warning,
+                    format!("Indirectly imported: {}", usage.name),
+                    usage.location.in_file(&parsed.original_path),
+                    None,
+                ),
+                _ => (),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn get_scope<'a>(
+    file: &'a Path,
+    files: &'a Map<PathBuf, Parsed>,
+    scopes: &mut Map<&'a Path, ScopeWip<'a>>,
+) -> Result<Scope<'a>, Error> {
+    match scopes.get(file) {
+        Some(ScopeWip::Current) => bail!("Recursive import of {}", file.display()),
+        Some(ScopeWip::Resolved(scope)) => return Ok(scope.clone()),
+        _ => (),
+    };
+    scopes.insert(file, ScopeWip::Current);
+
+    let parsed_file = files.get(file).ok_or_else(|| {
+        format_err!(
+            "List of elements in scope of file {} was requested, \
+             but not available due to previous import error",
+            file.display()
+        )
+    })?;
+
+    let mut scope = Scope::default();
+
+    for import in &parsed_file.imports {
+        let nested = get_scope(&import, files, scopes)?;
+        scope.directly_imported.extend(&nested.defined);
+        scope.all.extend(&nested.all);
+        scope.files.extend(&nested.files);
+    }
+
+    for definition in &parsed_file.definitions {
+        scope.defined.insert(&definition.name);
+        scope.all.insert(&definition.name);
+    }
+
+    scope.files.insert(file);
+
+    scopes.insert(file, ScopeWip::Resolved(scope.clone()));
+
+    Ok(scope)
 }
