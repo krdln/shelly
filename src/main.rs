6d32c7f60a69d7ace8ccc16fd1fe27cad13ef2b2
Michał Krasnoborski <mkrdln@gmail.com>
2018-10-05 23:49:00 +0200

    Underline precise error span with squiggles

--- src/main.rs
+++ src/main.rs
@@ -168,12 +168,16 @@ impl shelly::Emitter for CliEmitter {
         let blue = Color::Blue.style().bold();
         let pipe = blue.paint("|");
 
-        let message_kind = match item.kind {
-            shelly::MessageKind::Error   => Color::Red.style().bold().paint("error"),
-            shelly::MessageKind::Warning => Color::Yellow.style().bold().paint("warning"),
+        let (accent_style, message_kind) = match item.kind {
+            shelly::MessageKind::Error   => (Color::Red.style().bold(), "error"),
+            shelly::MessageKind::Warning => (Color::Yellow.style().bold(), "warning"),
         };
 
-        println!("{}: {}", message_kind, Style::new().bold().paint(item.message));
+        println!(
+            "{}: {}",
+            accent_style.paint(message_kind),
+            Style::new().bold().paint(item.message)
+        );
 
         offset();
         println!(
@@ -191,12 +195,31 @@ impl shelly::Emitter for CliEmitter {
 
             let line = span.start.find_line(&item.location.source);
             println!("{} {} {}", blue.paint(&line_no), pipe, line);
+
+            // Now, let's print squiggles
+
+            offset();
+            print!(" {} ", pipe);
+
+            let offset = span.start.col as usize - 1;
+            let len = (span.end.byte - span.start.byte) as usize;
+            let len = if offset + len <= line.len() { len } else { line.len() - offset };
+            let width = line[offset..][..len].chars().count();
+
+            // Print space before squiggles.
+            // We're printing it char-by-char to handle tabs the same way as original line.
+            for c in line[..offset].chars() {
+                if c.is_whitespace() {
+                    print!("{}", c);
+                } else {
+                    print!(" ");
+                }
+            }
+
+            println!("{}", accent_style.paint("^".repeat(width)));
         }
 
         if let Some(notes) = item.notes {
-            offset();
-            println!(" {}", pipe);
-
             for line in notes.lines() {
                 offset();
                 println!(" {} {}", blue.paint("="), line);
